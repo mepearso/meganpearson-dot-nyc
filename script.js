90f0bbf98cb6929360a751ff67369728c9fed68a
Megan Pearson <megan.e.pearson@gmail.com>
2015-10-12 20:33:03 -0400

    project details show/hide

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("linkage has occurred");
+
+$('article').click(function(){
+	// hide other articles details section
+	var detailsToHide = $(this).siblings().children('.details');
+	detailsToHide.slideUp(500);
+	//show clicked articles details section
+	$(this).children('.details').slideDown(500);
+});
+
